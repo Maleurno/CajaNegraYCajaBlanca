ecdddfcbbf9f0675dc485404f06aedcbfd0f84b1
maleurno <danielsivale@gmail.com>
2017-05-10 16:45:34 +0200

    Terminado Ciclomatica.java
    Terminado los test que hay introducido en Ciclomatica.java

--- src/TesteoMinMax/Ciclomatica.java
+++ src/TesteoMinMax/Ciclomatica.java
@@ -2,6 +2,38 @@ package TesteoMinMax;
 
 import org.junit.*;
 
+import MinMax_package.EmptyNumberException;
+import MinMax_package.MinMax;
+
 public class Ciclomatica {
+	private int[] xs;
+	
+	@Before
+	public void inicio(){
+		xs = null;
+	}
+	
+	@Test
+	public void primerCamino(){
+		xs =new int[] {2,1};
+		MinMax.minMax(xs);
+	}
+	
+	@Test(expected = EmptyNumberException.class)
+	public void segundoCamino(){
+		MinMax.minMax(xs);
+	}
+	
+	@Test
+	public void tercerCamino(){
+		xs =new int[] {1};
+		MinMax.minMax(xs);
+	}
+	
+	@Test
+	public void cuartoCamino(){
+		xs =new int[] {2,3,0};
+		MinMax.minMax(xs);
+	}
 
 }
