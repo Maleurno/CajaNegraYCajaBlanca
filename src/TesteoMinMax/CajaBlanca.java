18fd0989f6694244a4c7292cc6dfeefd1e20a2c7
maleurno <danielsivale@gmail.com>
2017-05-10 13:39:14 +0200

    Creo la clase CajaBlanca.java/Create the class CajaBlanca.java
    Se crea la clase donde se ejecutaran las sentencias para comprobar los test de caja blanca.

---
+++ src/TesteoMinMax/CajaBlanca.java
@@ -0,0 +1,30 @@
+package TesteoMinMax;
+
+import org.junit.*;
+
+import MinMax_package.EmptyNumberException;
+import MinMax_package.MinMax;
+
+public class CajaBlanca {
+	
+	@Test
+	public void statementCoverage(){
+		
+	}
+	
+	@Test
+	public void decisionCoverage(){
+		
+	}
+	
+	@Test
+	public void conditionCoverage(){
+		
+	}
+	
+	@Test
+	public void decisionConditionCoverage(){
+		
+	}
+
+}
