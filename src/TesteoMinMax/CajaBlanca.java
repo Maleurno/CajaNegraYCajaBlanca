1c93a6882677ae210d4e5653b1999e84cb723807
maleurno <danielsivale@gmail.com>
2017-05-10 16:25:30 +0200

    Terminado la aplicación CajaBlanca.java
    Terminado las opciones de caja Blanca.

--- src/TesteoMinMax/CajaBlanca.java
+++ src/TesteoMinMax/CajaBlanca.java
@@ -1,30 +1,47 @@
 package TesteoMinMax;
 
 import org.junit.*;
-
-import MinMax_package.EmptyNumberException;
 import MinMax_package.MinMax;
 
 public class CajaBlanca {
+	private int[] xs;
+	
+	@Before
+	public void inicio(){
+		xs = null;
+	}
+	
+	@After
+	public void fin(){
+		inicio();
+	}
 	
 	@Test
 	public void statementCoverage(){
-		
+		xs =new int[] {2,1,3,0};
+		MinMax.minMax(xs);
 	}
 	
 	@Test
 	public void decisionCoverage(){
-		
+		xs =new int[] {0,1};
+		MinMax.minMax(xs);
+		xs =null;
+		MinMax.minMax(xs);		
 	}
 	
 	@Test
 	public void conditionCoverage(){
-		
+		xs =new int[] {0,1};
+		MinMax.minMax(xs);
+		xs = null;
+		MinMax.minMax(xs);
 	}
 	
 	@Test
 	public void decisionConditionCoverage(){
-		
+		decisionCoverage();
+		conditionCoverage();
 	}
 
 }
